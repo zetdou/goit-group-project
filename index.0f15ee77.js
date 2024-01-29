92ffe810b4de37414ea6d117b7b3311e51913803
DawidKrupski <108096793+DawidKrupski@users.noreply.github.com>
2024-01-29 06:32:50 +0000

    Deploying to gh-pages from @ zetdou/goit-group-project@91c9bc5c94eecb426e943a633af18741f7368abd 🚀

---
+++ index.0f15ee77.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[data-modal-open]"),openModalBtn2:document.querySelector("[data-modal-open2]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.openModalBtn2.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.0f15ee77.js.map
